b77a81f8088a4501750fe222109b38d5e86e6438
Mikhail <eldoctor1313@gmail.com>
2026-08-24 01:37:23 +0300

    добавлены заглушки

--- fit_life.py
+++ fit_life.py
@@ -5,23 +5,33 @@
 # TODO: Спроси у пользователя имя и сохрани в переменную user_name
 # TODO: Спроси возраст и сохрани в user_age (не забудь преобразовать в число)
 print('Вас приветствует Fit Life!')
-user_name = input('Как Вас зовут?')
-user_age = int(input('Сколько Вам лет?'))
+user_name = input('Как Вас зовут? ')
+user_age = int(input('Сколько Вам лет? '))
+def age_compare():
+    pass
+
 
 # 2. Сбор данных
+def height_compare():
+    pass
+
+
 # TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
 # TODO: Запроси рост (в метрах) и сохрани в user_height (тип float)
-user_weight = float(input('Сколько Вы весите?(в кг.)'), 2)
-user_height = float(input('Ваш рост?(в метрах)'), 2)
+user_weight = float(input('Сколько Вы весите?(в кг.) '))
+user_height = float(input('Ваш рост?(в метрах) '))
 
+    
 # 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
 # Формула ИМТ: вес разделить на (рост в квадрате)
 # TODO: Рассчитай bmi (Индекс массы тела)
 bmi = user_weight / (user_height ** 2)
-round(bmi, 1)
+def bmi_result():
+    pass
+
 
 # Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
+# TODO: Рассчитай water_neededVb
 water_needed = (user_weight * 30) / 1000
 
 # 4. Вывод красивого результата
@@ -41,6 +51,6 @@ def get_year_word(user_age):
 
 print(f'Привет, {user_name}!')
 print(f'В {user_age} {get_year_word(user_age)}:')
-print(f'Твой ИМТ: {bmi}')
+print(f'Твой ИМТ: {round(bmi, 1)}')
 print(f'Рекомендуемая норма воды: {water_needed} л.')
 print("Расчет окончен. Будьте здоровы!")
